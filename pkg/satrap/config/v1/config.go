459a3f179940933d3882c401dae4bac1d6add543
AFz <0xafz@proton.me>
2025-08-22 01:22:23 +0330

    refactor: move config structs to versioned packages

--- pkg/satrap/config/v1/config.go
+++ pkg/satrap/config/v1/config.go
@@ -1,13 +1,24 @@
-package config
+package v1
 
 import "time"
+
+type TLSConfig struct {
+	Enabled  bool   `mapstructure:"enabled" yaml:"enabled"`
+	CertFile string `mapstructure:"certFile" yaml:"certFile"`
+	KeyFile  string `mapstructure:"keyFile" yaml:"keyFile"`
+}
+type ClusterConfig struct {
+	Enabled bool   `mapstructure:"enabled" yaml:"enabled"`
+	Server  string `mapstructure:"server" yaml:"server"`
+	Token   string `mapstructure:"token" yaml:"token"`
+}
 
 type XrayConfig struct {
 	Address string `mapstructure:"address" yaml:"address"`
 	Port    uint16 `mapstructure:"port" yaml:"port"`
 }
 
-type SatrapConfig struct {
+type Config struct {
 	NodeID                    string        `mapstructure:"nodeID" yaml:"nodeID"`
 	Address                   string        `mapstructure:"address" yaml:"address"`
 	Port                      uint16        `mapstructure:"port" yaml:"port"`
@@ -17,4 +28,5 @@ type SatrapConfig struct {
 	Xray                      XrayConfig    `mapstructure:"xray" yaml:"xray"`
 	Cluster                   ClusterConfig `mapstructure:"cluster" yaml:"cluster"`
 	NodeStatusUpdateFrequency time.Duration `mapstructure:"nodeStatusUpdateFrequency" yaml:"nodeStatusUpdateFrequency"`
+	InboundTTLCheckPeriod     time.Duration `mapstructure:"inboundTTLCheckPeriod" yaml:"inboundTTLCheckPeriod"`
 }
