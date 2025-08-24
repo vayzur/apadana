5f48e4750a5ec0465d18cd5ba8227cda57a37815
AFz <0xafz@proton.me>
2025-08-24 16:50:42 +0330

    feat(config): add cluster config and concurrent node syncs parameter

--- pkg/spasaka/config/v1/types.go
+++ pkg/spasaka/config/v1/types.go
@@ -2,8 +2,16 @@ package v1
 
 import "time"
 
+type ClusterConfig struct {
+	Enabled bool   `mapstructure:"enabled" yaml:"enabled"`
+	Server  string `mapstructure:"server" yaml:"server"`
+	Token   string `mapstructure:"token" yaml:"token"`
+}
+
 type SpasakaConfig struct {
+	Cluster                ClusterConfig `mapstructure:"cluster" yaml:"cluster"`
 	EtcdEndpoints          []string      `mapstructure:"etcd" yaml:"etcd"`
+	ConcurrentNodeSyncs    int           `mapstructure:"concurrentNodeSyncs" yaml:"concurrentNodeSyncs"`
 	NodeMonitorPeriod      time.Duration `mapstructure:"nodeMonitorPeriod" yaml:"nodeMonitorPeriod"`
 	NodeMonitorGracePeriod time.Duration `mapstructure:"nodeMonitorGracePeriod" yaml:"nodeMonitorGracePeriod"`
 }
