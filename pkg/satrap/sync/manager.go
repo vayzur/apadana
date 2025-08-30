67c0c3b9b23b9e24da05a5ba87c7def6c27922b9
AFz <0xafz@proton.me>
2025-08-31 00:18:21 +0330

    feat(syncManager): add new concurrency fields and update NewSyncManager constructor

--- pkg/satrap/sync/manager.go
+++ pkg/satrap/sync/manager.go
@@ -8,19 +8,34 @@ import (
 )
 
 type SyncManager struct {
-	xrayClient             *xray.Client
-	apadanaClient          *apadana.Client
-	syncFrequency          time.Duration
-	concurrentInboundSyncs int32
-	concurrentGCSyncs      int32
-	concurrentExpireSyncs  int32
-	concurrentUserSyncs    int32
+	xrayClient                   *xray.Client
+	apadanaClient                *apadana.Client
+	syncFrequency                time.Duration
+	concurrentInboundSyncs       int32
+	concurrentInboundExpireSyncs int32
+	concurrentInboundGCSyncs     int32
+	concurrentUserSyncs          int32
+	concurrentUserExpireSyncs    int32
 }
 
-func NewSyncManager(xrayClient *xray.Client, apadanaClient *apadana.Client, syncFrequency time.Duration) *SyncManager {
+func NewSyncManager(
+	xrayClient *xray.Client,
+	apadanaClient *apadana.Client,
+	syncFrequency time.Duration,
+	concurrentInboundSyncs,
+	concurrentInboundExpireSyncs,
+	concurrentInboundGCSyncs,
+	concurrentUserSyncs,
+	concurrentUserExpireSyncs int32,
+) *SyncManager {
 	return &SyncManager{
-		xrayClient:    xrayClient,
-		apadanaClient: apadanaClient,
-		syncFrequency: syncFrequency,
+		xrayClient:                   xrayClient,
+		apadanaClient:                apadanaClient,
+		syncFrequency:                syncFrequency,
+		concurrentInboundSyncs:       concurrentInboundSyncs,
+		concurrentInboundExpireSyncs: concurrentInboundExpireSyncs,
+		concurrentInboundGCSyncs:     concurrentInboundGCSyncs,
+		concurrentUserSyncs:          concurrentUserSyncs,
+		concurrentUserExpireSyncs:    concurrentUserExpireSyncs,
 	}
 }
